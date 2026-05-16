d3390845ece42ce70d16fe89e70e9c1c67b947ae
leurianny <leuriannysalassosa32@gmail.com>
2026-05-16 13:44:34 -0400

    form configuracion listo

---
+++ CapaPresentacion/FormConfiguracion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion
+{
+    public partial class FormConfiguracion : Form
+    {
+        public FormConfiguracion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSeleccioneLogo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
